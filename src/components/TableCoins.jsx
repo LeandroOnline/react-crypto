ae421108862ca6ae92e9a48acc4026ac3e91854d
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2022-12-14 00:21:50 -0300

    m

--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import CoinRow from './CoinRow';
+import React from "react";
+import CoinRow from "./CoinRow";
 
-const TableCoins = ({coins, search}) => {
-    const titles=["#","Coin","Price","Price Change","24h Volume"];
-    
-    const filterCrypto= coins?.filter(coin => coin.name.toLowerCase().includes(search?.toLowerCase()));
+const TableCoins = ({ coins, search }) => {
+  const titles = ["#", "Coin", "Price", "Price Change", "24h Volume"];
 
-    return (
-        <table className='table table-dark mt-4 table-hover'>
-            <thead>
-                <tr>
-                    {titles.map((t,index)=>(<td key={index}>{t}</td>))}
-                </tr>
-            </thead>
-            
-            <tbody>
-                    {filterCrypto?.map((coin,index) => (
-                        <CoinRow coin={coin} key={index} index={index}/>
-                    ))}
-            </tbody>
-        </table>
-    );
-}
+  const filterCrypto = coins?.filter((coin) =>
+    coin.name.toLowerCase().includes(search?.toLowerCase()) | 
+    coin.symbol.toLowerCase().includes(search?.toLowerCase())
+  );
+
+  return (
+    <table className="table table-dark mt-4 table-hover">
+      <thead>
+        <tr>
+          {titles.map((t, index) => (
+            <td key={index}>{t}</td>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {filterCrypto?.map((coin, index) => (
+          <CoinRow coin={coin} key={index} index={index} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default TableCoins;
